eae384c6851dd1001fd636c01067ec967fcf12c5
marcelooliveira <mclricardo@gmail.com>
2019-06-06 16:53:40 -0300

    ASPNETCore2-Parte3

---
+++ antes/Item04/CasaDoCodigo/Models/Pedido.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    //MELHORIA: 5) arquivo modelo.cs foi quebrado em vários arquivos
+    //veja o diagrama
+    public class Pedido : BaseModel
+    {
+        public Pedido()
+        {
+            Cadastro = new Cadastro();
+        }
+
+        //TAREFA 02: CRIAR CONSTRUTOR COM ID DO CLIENTE
+
+        //TAREFA 03: ADICIONAR MIGRAÇÃO
+        //TAREFA 04: APLICAR MIGRAÇÃO
+        
+        public Pedido(Cadastro cadastro)
+        {
+            Cadastro = cadastro;
+        }
+
+        public List<ItemPedido> Itens { get; private set; } = new List<ItemPedido>();
+
+        [ForeignKey("CadastroId")]
+        public int CadastroId { get; set; }
+        [Required]
+        public virtual Cadastro Cadastro { get; private set; }
+
+        //TAREFA 01: ADICIONAR ID DO CLIENTE AO PEDIDO
+    }
+}
---
+++ antes/Item04/CasaDoCodigo/Repositories/HttpHelper.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace CasaDoCodigo
+{
+    //TAREFA 05: INJETAR UserManager PARA OBTER clienteId
+    public class HttpHelper : IHttpHelper
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+        public IConfiguration Configuration { get; }
+
+        public HttpHelper(IHttpContextAccessor contextAccessor, IConfiguration configuration)
+        {
+            this.contextAccessor = contextAccessor;
+            Configuration = configuration;
+        }
+
+        public int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        public void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
+        }
+
+        public void ResetPedidoId()
+        {
+            contextAccessor.HttpContext.Session.Remove("pedidoId");
+        }
+    }
+}
